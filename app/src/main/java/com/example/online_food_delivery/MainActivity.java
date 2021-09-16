2dd1345be647cbe8fa92dbd8cecd5f010cf71ece
ashumig <ashaniisu99@gmail.com>
2021-09-17 02:08:05 +0700

    initial

--- app/src/main/java/com/example/online_food_delivery/MainActivity.java
+++ app/src/main/java/com/example/online_food_delivery/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //hey Food delivery App
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
